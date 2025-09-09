d29e57290634c1d160e0b05eeba8a0a5027ccda6
Paul <69437294+Escbrk@users.noreply.github.com>
2025-09-08 18:44:20 -0600

    upd

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const modalOpenBtn = document.querySelector(".comments-btn");
-const modalCloseBtn = document.querySelector(".modal-close-btn");
-const modalOverlay = document.querySelector(".modal-overlay");
+// const modalOpenBtn = document.querySelector(".comments-btn");
+// const modalCloseBtn = document.querySelector(".modal-close-btn");
+// const modalOverlay = document.querySelector(".modal-overlay");
 
-modalOpenBtn.addEventListener("click", () => {
-  modalOverlay.classList.add("is-open");
+// modalOpenBtn.addEventListener("click", () => {
+//   modalOverlay.classList.add("is-open");
+// });
+
+// modalCloseBtn.addEventListener("click", () => {
+//   modalOverlay.classList.remove("is-menu-open");
+// });
+
+//!++++++++++++++++++++++++++++
+
+const mobileOpenBtn = document.querySelector(".burger-menu");
+const mobileCloseBtn = document.querySelector(".close-mobile-menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+mobileOpenBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("is-menu-open");
 });
 
-modalCloseBtn.addEventListener("click", () => {
-  modalOverlay.classList.remove("is-open");
+mobileCloseBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("is-menu-open");
 });
